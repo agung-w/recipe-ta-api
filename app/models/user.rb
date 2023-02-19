154475c1bbeb5e905e703027b34bcb720f7b85ac
agung-w <suronokz3471@yahoo.com>
2023-02-19 23:35:45 +0700

    Add some more validation and fix password and username attribute validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
     validates :name, length: { in: 3..60 }, format: { with: /\A[a-zA-Z]+\z/,message: "only allows letters" }
-    validates :email, email:true
+    validates :email, email:true ,uniqueness: true
+    validates :password, length: {in: 6..60}
+    validates :username,length: { in: 3..60 }, uniqueness: true,format: { with: /\A[a-zA-Z0-9_-]+\z/,message: "only allows letters, numbers, - and _" }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,9 +2,15 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   context "Name attribute" do
-    it "length must be more than 3" do
-      user=build(:user,name:"a")
-      expect(user).to_not be_valid
+    it "must contain at least 3 char and maximum 60 char" do
+      user1=build(:user,name:"abc")
+      user2=build(:user,name:"a")
+      user3=build(:user,name:"a"*60)
+      user4=build(:user,name:"a"*61)
+      expect(user1).to be_valid
+      expect(user2).to_not be_valid
+      expect(user3).to be_valid
+      expect(user4).to_not be_valid
     end
     it "must not contain number or special character" do
       user1=build(:user,name:"a321")
@@ -30,11 +36,15 @@ RSpec.describe User, type: :model do
   end
 
   context "Password attribute" do
-    it "length must be more than 6" do
+    it "must contain at least 6 char and maximum 60 char" do
       user1=build(:user,password:"123456")
       user2=build(:user,password:"a#b")
+      user3=build(:user,password:"a"*60)
+      user4=build(:user,password:"a"*61)
       expect(user1).to be_valid
       expect(user2).to_not be_valid
+      expect(user3).to be_valid
+      expect(user4).to_not be_valid
     end
   end
 
@@ -44,6 +54,22 @@ RSpec.describe User, type: :model do
       user2=build(:user,username:"agung")
       expect(user1).to be_valid
       expect(user2).to_not be_valid
+    end
+    it "must only include letter/number/-/_" do
+      user1=create(:user,username:"agung-w1")
+      user2=build(:user,username:"agung*")
+      expect(user1).to be_valid
+      expect(user2).to_not be_valid
+    end
+    it "must contain at least 3 char and maximum 60 char" do
+      user1=build(:user,username:"aaa")
+      user2=build(:user,username:"aa")
+      user3=build(:user,username:"a"*60)
+      user4=build(:user,username:"a"*61)
+      expect(user1).to be_valid
+      expect(user2).to_not be_valid
+      expect(user3).to be_valid
+      expect(user4).to_not be_valid
     end
   end
 
