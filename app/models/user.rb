de4102fcac7462dc51697d3bf29c2ef03d2925fd
agung-w <suronokz3471@yahoo.com>
2023-02-20 21:27:22 +0700

    Secure password for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
-    validates :name, length: { in: 3..60 }, format: { with: /\A[a-zA-Z]+\z/,message: "only allows letters" }
-    validates :email, email:true ,uniqueness: true
-    validates :password, length: {in: 6..60}
-    validates :username,length: { in: 3..60 }, uniqueness: true,format: { with: /\A[a-zA-Z0-9_-]+\z/,message: "only allows letters, numbers, - and _" }
+  has_secure_password
+  validates :name, length: { in: 3..60 }, format: { with: /\A[a-zA-Z]+\z/,message: "only allows letters" }
+  validates :email, email:true ,uniqueness: true
+  validates :password, length: {in: 6..60}
+  validates :username,length: { in: 3..60 }, uniqueness: true,format: { with: /\A[a-zA-Z0-9_-]+\z/,message: "only allows letters, numbers, - and _" }
+
+
 end
