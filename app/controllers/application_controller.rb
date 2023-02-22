ee3d6ca3332a0e731fd3b393b84af28ff7eae155
agung-w <suronokz3471@yahoo.com>
2023-02-22 21:21:29 +0700

    Fix login and register issues after adding secure password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,9 +14,9 @@ class ApplicationController < ActionController::Base
 
   def payload(user)
     {
-        'email':user.email,
-        'name':user.name,
-        'iat':Time.now.to_i
+      'email':user.email,
+      'name':user.name,
+      'iat':Time.now.to_i
     }
   end
 end
