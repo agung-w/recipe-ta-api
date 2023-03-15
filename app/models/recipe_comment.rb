66546b8c4b8a4df2653f34915c52cbd6500f9188
agung-w <suronokz3471@yahoo.com>
2023-03-15 22:19:01 +0700

    Change recipe comment as_json

--- app/models/recipe_comment.rb
+++ app/models/recipe_comment.rb
@@ -3,4 +3,7 @@ class RecipeComment < ApplicationRecord
   belongs_to :recipe
   validates :content, length: { in: 1..3000 }
 
+  def as_json(options=nil)
+    super({ only: [:content,:updated_at],include: [{user:User.profile_attr}]}.merge(options || {}))
+  end
 end
